ce4416d623f74516ced46740adcae6eeb4be80e3
Dhinesh-23 <sankarsnr123@gmail.com>
2026-05-16 22:50:42 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-# main.py
+from http.server import BaseHTTPRequestHandler
 
-def handler(request):
-    return {
-        "statusCode": 200,
-        "body": "Hello from Vercel"
-    }
+class handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/plain")
+        self.end_headers()
+        self.wfile.write(b"Hello from Vercel Python!")
